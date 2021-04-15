9f534644a445c01656648cb04991c4a28dac3455
1HuseyinGunes <hsyngunes.17@gmail.com>
2021-04-16 00:17:02 +0300

    Token a ait verileri tutan Token Sınıfı oluşturuldu

---
+++ ITS.CORE/Token/Token.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITS.CORE.Token
+{
+   public class Token
+    {
+        public string AccesToken { get; set; }
+        public DateTime AccesTokenLifeTime { get; set; }
+
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenLifeTime { get; set; }
+    }
+}
