e3e5fba95c1c835b06256a8d5af52fd02ef5faac
1HuseyinGunes <hsyngunes.17@gmail.com>
2021-04-16 21:48:10 +0300

    IUserService interface i oluşturuldu

---
+++ ITS.CORE/Dto/CavusDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITS.CORE.Dto
+{
+   public  class CavusDto //Cliente dönecek olan kullanıcı bilgilerini içiren sınıftır
+    {
+        public string CavusId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int GrupId { get; set; }
+    }
+}
