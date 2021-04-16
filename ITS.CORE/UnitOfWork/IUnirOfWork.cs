045ff292529dbb59f1246f500d54bc49cf5ead79
1HuseyinGunes <hsyngunes.17@gmail.com>
2021-04-16 21:50:51 +0300

    IUnitOfWork interface i oluşturuldu

---
+++ ITS.CORE/UnitOfWork/IUnirOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITS.CORE.UnitOfWork
+{
+   public interface IUnirOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
